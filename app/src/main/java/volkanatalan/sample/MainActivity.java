1029cc2d56bd08a38e2bcf5506bcadc3e41db631
Volkan ATALAN <volkanatalan35@gmail.com>
2018-07-30 16:40:18 +0300

    border color set

--- app/src/main/java/volkanatalan/sample/MainActivity.java
+++ app/src/main/java/volkanatalan/sample/MainActivity.java
@@ -3,11 +3,16 @@ package volkanatalan.sample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import volkanatalan.circleimageview.views.CircleImageView;
+
 public class MainActivity extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+  
+    CircleImageView circleImageView = findViewById(R.id.circleImageView);
+    circleImageView.setBorderColor(getResources().getColor(R.color.image_border));
   }
 }
